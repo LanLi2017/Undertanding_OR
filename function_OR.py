ee24630a17173139f31ecfcc4c0838c022ce2ba5
Lan Li <lilan.scut@gmail.com>
2020-03-23 19:35:23 -0500

    TBD

--- function_OR.py
+++ function_OR.py
@@ -5,14 +5,6 @@ import numpy as np
 import pandas as pd
 
 
-class cell:
-    def __init__(self):
-        pass
-
-
-    pass
-
-
 class Operation:
     # 定义了什么操作：传入一个数据集， 通过这个大函数 能得到另外一个数据集
     # 对于任意一个OPERATION集合路径，都可以调用 split-simplify-.py
@@ -35,10 +27,18 @@ class Operation:
 
     def __init__(self):
         self.D=pd.DataFrame() # initialize new data frame
+        self.dependency=list()
 
     def pd_csv(self,data_p):
         self.D = pd.read_csv(data_p)
 
+    def row_idx_change(self,col_name, expression):
+        # return the changed cell with row index
+        for index, value in self.D[col_name].items():
+            if value != value.expression():
+                return index
+
+    # row level
     def base_del_row_op(self,row_idx):
         '''
         # copy row (value + position)
@@ -48,7 +48,10 @@ class Operation:
         :return: output dataset/ stringIO / List
         '''
         self.D = self.D.drop(row_idx)
+        record = { 'row': f'- {row_idx}'}
+        self.dependency.append(record)
 
+    # column level
     def base_del_col_op(self,drop_col):
         ''' A (COUNT) B () C ()
         A:3 B:5 C:7
@@ -62,11 +65,15 @@ class Operation:
         :return:
         '''
         self.D = self.D.drop(columns=drop_col)
+        record = {'column': f'- {drop_col}'}
+        self.dependency.append(record)
 
     def base_add_col_op(self,new_col,old_col,insert_idx,copy=True,*add_col_val):
         # new column name
         # old column name
         # new column position
+        record = {'column': f'+ {new_col}'}
+        self.dependency.append(record)
         if copy:
             # copy the value from the old column
             new_col_val=self.D[old_col]
@@ -83,14 +90,21 @@ class Operation:
         # old_col: old position column name
         # copy add
         self.base_add_col_op(new_col,old_col,insert_idx,copy=True)
+        record1 = {'column':f'+ {new_col}'}
+        self.dependency.append(record1)
         # delete
         self.base_del_col_op(old_idx)
+        record2 = {'column': f'- {old_col}'}
+        self.dependency.append(record2)
 
     def split_col_op(self,old_col,regex=',',keep_old=True):
         # split
         new_df = pd.concat([self.D[old_col].str.split(regex, expand=True)], axis=1,keys=[old_col])
         new_df.columns = new_df.columns.map(lambda x: '_'.join((x[0], str(x[1]))))
         new_df = new_df.replace({'': np.nan, None: np.nan})
+        for col in new_df.columns:
+            record = {'column':f'+ {col}'}
+            self.dependency.append(record)
         if keep_old:
             # the whole table + new generated columns
             self.D = pd.concat([self.D, new_df],axis=1)
@@ -99,20 +113,62 @@ class Operation:
         else:
             # the whole table - old_col +new generated columns
             self.base_del_col_op(old_col)
+            record1 = {'column': f'-{old_col}'}
+            self.dependency.append(record1)
             self.D = pd.concat([self.D, new_df], axis=1)
 
     def rename_col_op(self,new_name,old_col,old_col_idx):
         # table + add/copy column(new name,same position) + delete old column
         self.base_add_col_op(new_name,old_col,old_col_idx,copy=True)
+        record1 = {'column': f'+ {new_name}'}
+        self.dependency.append(record1)
         self.base_del_col_op(old_col)
+        record2 = {'column': f'- {old_col}'}
+        self.dependency.append(record2)
 
-    def output_data(self,name):
-        self.D.to_csv(f'{name}.csv',index=False)
-        return self.D
+    # Cell level
+    def cell_mass_edit(self,expression,col_name):
+        '''
+        expression: apply function
+        col_name: apply column
+        row_idx: changed row
+        dependency: row_index, column_name
+        '''
+        # cluster-edit
+        # common transformations
+        if expression == 'uppercase':
+            for index, value in self.D[col_name].items():
+                if value == value.upper():
+                    pass
+                else:
+                    record = {'cell': (index, col_name)}
+                    self.dependency.append(record)
+                    return index
+        elif expression == 'lowercase':
+            for index, value in self.D[col_name].items():
+                if value == value.lower():
+                    # no change
+                    pass
+                else:
+                    record = {'cell': (index, col_name)}
+                    self.dependency.append(record)
+                    return index
+        elif expression == 'number':
+            # here use double
+            for index, value in self.D[col_name].items():
+                try:
+                    if value == int(value):
+                        pass
+                    else:
+                        record = {'cell': (index, col_name)}
+                        self.dependency.append(record)
+                        return index
+                except ValueError:
+                    pass
 
-    def cell_pos(self):
-        # cell index
-        pass
+    def cell_single_edit(self,row_idx, col_name):
+        record = {'cell': (row_idx, col_name)}
+        self.dependency.append(record)
 
     def split_simplify_op(self):
         # 对于任何一个operation， 首先进行拆分简化，通过排序来比较路径是否相等
@@ -126,6 +182,10 @@ class Operation:
 
     def topology_(self):
         pass
+
+    def output_data(self, name):
+        self.D.to_csv(f'{name}.csv', index=False)
+        return self.D
 
 
 def main():
@@ -147,15 +207,15 @@ def main():
     4. del column "color_style_copy"
     5. rename column "id" to "uID"
     
-    '''
-    OPS2=Operation()
-    OPS2.pd_csv(data_p)
-    OPS2.rename_col_op('uID','id',0)
-    OPS2.base_add_col_op('color_style_copy','color_style',3)
-    OPS2.base_del_row_op(2)
-    OPS2.split_col_op('color_style_copy','_',keep_old=False)
-
-    df2=OPS2.output_data('OPS2')
+    # '''
+    # OPS2=Operation()
+    # OPS2.pd_csv(data_p)
+    # OPS2.rename_col_op('uID','id',0)
+    # OPS2.base_add_col_op('color_style_copy','color_style',3)
+    # OPS2.base_del_row_op(2)
+    # OPS2.split_col_op('color_style_copy','_',keep_old=False)
+    #
+    # df2=OPS2.output_data('OPS2')
 
     '''
     op_list2:
@@ -184,7 +244,7 @@ def main():
     ?
     
     '''
-    print(df1.equals(df2))
+    # print(df1.equals(df2))
 
 
 if __name__=='__main__':
